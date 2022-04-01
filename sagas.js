2a1f1a9327a5979ad8ceea0b07e675327d4c4ca4
McCambley <jakemccamb16@gmail.com>
2022-04-01 16:58:56 -0400

    Dev: create tests

--- sagas.js
+++ sagas.js
@@ -10,7 +10,7 @@ export function* helloSaga() {
 
 // Our worker Saga: will perform the async increment task
 export function* incrementAsync() {
-  yield delay(1000);
+  yield call(delay, 1000);
   yield put({ type: "INCREMENT" });
 }
 
